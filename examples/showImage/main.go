cc54062825352569e7063fced249aedf36db41db
Edward Hesketh <inbox@edwardh.dev>
2025-02-24 00:25:16 +0000

    chore(showImage): tidy example comments

--- examples/showImage/main.go
+++ examples/showImage/main.go
@@ -23,54 +23,49 @@ func main() {
 		fmt.Printf("Error listening for connections: %v", err)
 		os.Exit(1)
 	}
-	// Close the listener when the application closes.
-	defer l.Close()
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	defer l.Close()
+	
 	for {
-		// Listen for an incoming connection.
+		// Wait for an incoming connection.
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Printf("Error accepting a connection: %v", err)
 			os.Exit(1)
 		}
-		// Handle connections in a new goroutine.
+		// Handle the connection in a new goroutine.
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
 	fmt.Println("New connection from:", conn.RemoteAddr())
-	// Setting up the connection to the display.
 	lc := wavesharecloud.NewLoggingConn(conn, false)
 
-	// Creating the display. If a password is required to unlock the display, here is where you would enter it.
-	// This automatically unlocks the display when created.
+	// If a password is required to unlock the display, it should be provided as the second argument, and the display will be unlocked.
 	// In this case, the display is not locked, so the password is not required.
 	display := wavesharecloud.NewDisplay(lc, "")
 
-	// Open the timages and decode them.
+	// Open and JPEG decode the test image into an 'image.Image'.
 	testPatternImage, err := openImage("testpattern.jpg")
 	if err != nil {
 		fmt.Printf("Error opening test pattern image: %v\n", err)
 	}
 
-	// Drawing the testpattern image to the display.
+	// Draw the testpattern image to the display.
 	err = display.SendImage(testPatternImage)
 	if err != nil {
 		fmt.Printf("Error sending testpattern image: %v\n", err)
 	}
 
-	// Shutdown the display.
-	err = display.Shutdown()
+	err := display.Shutdown()
 	if err != nil {
 		fmt.Printf("Error shutting down: %v\n", err)
 	}
-	// Close the connection.
 	display.Disconnect()
 }
 
 func openImage(path string) (img image.Image, err error) {
-	// Open an image and decode it.
 	imageFile, err := os.Open(path)
 	if err != nil {
 		return nil, err
